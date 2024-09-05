464d04ce8de50ecaf7c540d5fa6a3d1a5622fb1e
Figby222 <figby222@outlook.com>
2024-09-05 03:53:24 -0700

    Set connectionString to DATABASE_URL
    Set connectionString to DATABASE_URL instead of
    DATABASE_CONNECTION_STRING

--- db/pool.mjs
+++ db/pool.mjs
@@ -2,5 +2,5 @@ import pg from "pg";
 const { Pool } = pg;
 
 export default new Pool({
-    connectionString: process.env.DATABASE_CONNECTION_STRING,
+    connectionString: process.env.DATABASE_URL,
 });
