0411d73cbda1385e5c8bd872599e6c273415a6f5
Figby222 <figby222@outlook.com>
2024-09-05 03:31:17 -0700

    Convert insertMessage() parameters to object
    Convert insertMessage() parameters to object

--- db/queries.mjs
+++ db/queries.mjs
@@ -1,6 +1,6 @@
 import pool from "./pool.mjs";
 
-async function insertMessage(username, text, added) {
+async function insertMessage({ username, text, added }) {
     await pool.query(`
         INSERT INTO messages (username, text, added)
         VALUES ($1, $2, $3)
